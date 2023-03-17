a2914199935515c604366aa4825961e4f9975976
Zekkkk <1114886299@qq.com>
2023-03-17 15:36:20 +0800

    数组模拟邻接表存储图，深度优先遍历

--- src/d8/图和树的存储与遍历_树的重心.java
+++ src/d8/图和树的存储与遍历_树的重心.java
@@ -1,28 +1,26 @@
 package d8;
 
-import java.io.*;
+import java.util.Scanner;
 
 public class 图和树的存储与遍历_树的重心 {
-    public static StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-    public static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
-    public static int N = 100010, M = N * 2;
-    public static int n;
+    public static int N = 100010;
+    public static int M = 2 * N;
+    public static int idx = 1;
     public static int[] h = new int[N];
-    public static int[] e = new int[M];
     public static int[] ne = new int[M];
-    public static int idx = 0;
-    public static boolean[] s = new boolean[N];
+    public static int[] e = new int[M];
+    public static boolean[] st = new boolean[N];
+    public static int n;
     public static int ans = N;
 
-    public static void main(String[] args) throws IOException {
-        for (int i = 0; i < N; i++)
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        for (int i = 0; i < N; i++) {
             h[i] = -1;
-
-        n = nextInt();
+        }
         for (int i = 0; i < n - 1; i++) {
-            int a, b;
-            a = nextInt();
-            b = nextInt();
+            int a = in.nextInt(), b = in.nextInt();
             add(a, b);
             add(b, a);
         }
@@ -32,39 +30,37 @@ public class 图和树的存储与遍历_树的重心 {
         System.out.println(ans);
     }
 
-    public static void add(int a, int b) {
-        // https://www.acwing.com/blog/content/4663/
-        // idx是边编号
-        // e[idx]是该边终点
-        // h[a]是该点，第一条边的编号
-        // ne[idx]是第idx边的下一条边
-        e[idx] = b;     //初始化该边终点为b
-        ne[idx] = h[a]; //下一条边是a点此时的第一条边
-        h[a] = idx;     //插入后，a点的第一条边变成了现在这条
-        idx++;          //边编号+1
-    }
-
     public static int dfs(int u) {
-        s[u] = true;
-
-        int sum = 1, res = 0;
-        for (int i = h[u]; i != -1; i = ne[i]) {
+        st[u] = true;
+        int sum = 1;
+        int res = 0;
+        for(int i = h[u]; i != -1; i = ne[i]) {
             int j = e[i];
-            if (!s[j]) {
-                int s = dfs(j);
-                res = Math.max(res, s);
-                sum += s;
+            if (!st[j]) {
+                int t = dfs(j);
+                sum += t;
+                res = Math.max(res, t);
             }
         }
-        res = Math.max(res, n - sum);
-
-        ans = Math.min(ans, res);
+        res = Math.max(res, n-res);
+        ans = Math.min(res, ans);
 
         return sum;
     }
 
-    public static int nextInt() throws IOException {
-        st.nextToken();
-        return (int) st.nval;
+    public static void add(int a, int b) {
+        e[idx] = b;
+        ne[idx] = h[a];
+        h[a] = idx++;
     }
 }
+//测试样例
+//9
+//1 2
+//1 7
+//1 4
+//2 8
+//2 5
+//4 3
+//3 9
+//4 6
